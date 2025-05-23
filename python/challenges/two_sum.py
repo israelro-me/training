ac13398ea0ba34848699c38188f7aa1d8b20f452
Israel Rodríguez Medina <israelro.me@outlook.com>
2025-05-23 14:16:37 -0600

    Python Scaffold

---
+++ python/challenges/two_sum.py
@@ -0,0 +1,8 @@
+def two_sum(nums: list[int], target: int) -> list[int]:
+    """
+    Given a list of numbers and a target number, return the indices of the two such that they add up to the target.
+
+    Source: https://leetcode.com/problems/two-sum
+    """
+    # Stub: I will do the actual implementation later.
+    return [1, 2]
