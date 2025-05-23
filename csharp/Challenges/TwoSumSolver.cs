ba68f5d00ffcd83cb22f66fa3cf5e20a64fabfe5
Israel Rodríguez Medina <israelro.me@outlook.com>
2025-05-23 13:02:55 -0600

    C# Scaffold

---
+++ csharp/Challenges/TwoSumSolver.cs
@@ -0,0 +1,16 @@
+﻿namespace Challenges;
+
+/// <summary>
+///     Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+///     target.
+///     Source: https://leetcode.com/problems/two-sum
+/// </summary>
+public static class TwoSumSolver
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        // This is just a stub implementation, the goal is to test all plumbing works: tests and GitHub pipelines.
+        // I will do the actual implementation under time tracking later.
+        return [1, 2];
+    }
+}
---
+++ csharp/Tests/TwoNumbersShouldSumToTarget.cs
@@ -0,0 +1,26 @@
+﻿using Challenges;
+using Shouldly;
+using Xunit;
+
+namespace Tests;
+
+public class TwoNumbersShouldSumToTarget
+{
+    public static TheoryData<int[], int, int[]> GetTwoSumCases()
+    {
+        return new TheoryData<int[], int, int[]>
+        {
+            { [2, 7, 11, 15], 9, [0, 1] },
+            { [3, 2, 4], 6, [1, 2] },
+            { [3, 3], 6, [0, 1] }
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTwoSumCases))]
+    public void ReturnsIndicesOfTwoNumbersThatAddToTarget(int[] input, int target, int[] expected)
+    {
+        var result = TwoSumSolver.TwoSum(input, target);
+        result.ShouldBe(expected);
+    }
+}
